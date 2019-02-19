d8997ddd38de0ef47c9f9ee4077725efe813bbf6
Rishabh Jain <mailrishabh1403@gmail.com>
2019-02-20 02:15:24 +0530

    feat: Add auth ui
    Resolves: #14

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-const App = () => <div>Howdy!!!</div>;
+import Page404 from './components/404/Page404';
+import Home from './components/Home/Home';
+
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="*" component={Page404} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => (
+  <div>In a Good Place!!!</div>
+);
+
+export default Home;
