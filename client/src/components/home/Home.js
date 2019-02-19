4d59f16d564e2db22fa3f5aec6e6e98788152bda
Rishabh Jain <mailrishabh1403@gmail.com>
2019-02-20 03:44:10 +0530

    add circle ci

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
 
 const Home = () => (
-  <div>In a Good Place!!!</div>
+  <Grid>
+    <Grid.Row centered columns={2}>
+      <Grid.Column textAlign='center'>
+        Sign Up
+      </Grid.Column>
+      <Grid.Column textAlign='center'>
+        Sign in
+      </Grid.Column>
+    </Grid.Row>
+    <Grid.Row textAlign='center' centered columns={2}>
+      <Grid.Column textAlign='center'>
+        Play as Guest
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
 );
 
 export default Home;
