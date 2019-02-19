82a0015ada0344dfcb0786f4100d77e7361d0c56
Rishabh Jain <mailrishabh1403@gmail.com>
2019-02-20 02:47:42 +0530

    add common header

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Page404 from './components/404/Page404';
-import Home from './components/Home/Home';
-
+import Home from './components/home/Home';
+import Header from './components/common/Header';
 
 const App = () => (
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="*" component={Page404} />
-    </Switch>
+
+
+    <Header>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="*" component={Page404} />
+      </Switch>
+    </Header>
+
   </BrowserRouter>
 );
 
